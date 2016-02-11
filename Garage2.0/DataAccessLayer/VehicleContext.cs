ac66a2b6adf3c5514ab455f5410f32d3c92b3bda
Emelie Rönnlund <emelieronnlund@gmail.com>
2016-02-11 12:57:23 +0100

    La till VehiclesController och VehicleContext

---
+++ Garage2.0/DataAccessLayer/VehicleContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Garage2._0.Models;
+
+namespace Garage2._0.DataAccessLayer
+{
+    public class VehicleContext : DbContext
+    {
+        public VehicleContext() : base("DefaultConnection") { }
+        DbSet<Vehicle> Garage { get; set; }
+
+        public System.Data.Entity.DbSet<Garage2._0.Models.Vehicle> Vehicles { get; set; }
+    }
+}
